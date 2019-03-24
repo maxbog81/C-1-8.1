8d349cca05e82eb30503254cca23421b3fa6ed0f
maxbog81 <maxbog@inbox.ru>
2019-03-25 01:27:07 +0300

    добавлено 5. **Написать программу-преобразователь из CSV в XML-файл с информацией о студентах (6 урок).

---
+++ FromCvsToXml/Program.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+//Богатов Максим
+//5.	**Написать программу-преобразователь из CSV в XML-файл с информацией о студентах (6 урок).
+namespace FromCvsToXml
+{
+    class Program
+    {
+
+        public Program()
+        {
+
+        }
+
+        static void SaveAsXmlFormat(List<Student> obj, string fileName)
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Student>));
+            // Создаем файловый поток(проще говоря, создаем файл)
+            Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            // В этот поток записываем сериализованные данные(записываем xml-файл)
+            xmlFormat.Serialize(fStream, obj);
+            fStream.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            //Создаем список студентов
+            List<Student> list = new List<Student>();
+            //DateTime dt = DateTime.Now;
+            StreamReader sr = new StreamReader("students_4.csv");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+                    //Добавляем в список новый экземляр класса Student
+                    Student t;
+                    t = new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), Convert.ToInt32(s[6]), int.Parse(s[7]), s[8]);
+                    list.Add(t);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            sr.Close();
+            SaveAsXmlFormat(list, "data.xml");
+
+            string sj = JsonConvert.SerializeObject(list);
+            File.WriteAllText("data.json",sj);
+            Console.WriteLine("Готово...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ FromCvsToXml/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FromCvsToXml
+{
+    [Serializable]
+    public class Student
+    {
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string univercity { get; set; }
+        public string faculty { get; set; }
+        public int course { get; set; }
+        public string department { get; set; }
+        public int group { get; set; }
+        public string city { get; set; }
+        public int age { get; set; }
+
+        public Student()
+        {
+
+        }
+
+    //Создаем конструктор
+    public Student(string firstName, string lastName, string univercity, string faculty, string department, int age, int course, int group, string city)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.univercity = univercity;
+            this.faculty = faculty;
+            this.department = department;
+            this.course = course;
+            this.age = age;
+            this.group = group;
+            this.city = city;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0,15}{1,15}{2,15}{3,15}{4,15}{5,15}{6,15}{7,5}{8,10}", firstName, lastName, univercity, faculty, department, age, course, group, city);
+        }
+    }
+}
